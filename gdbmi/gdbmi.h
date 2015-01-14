e23327b4599f788345b78600e51aeb35ffd265a6
r0nk <samuelshadethomas@gmail.com>
2015-01-13 22:57:16 -0500

    I've decided to focus on other projects and leave this one as a toy. I've removed the attempts at porting, and I'm no longer working on it.

--- gdbmi/gdbmi.h
+++
@@ -1,7 +0,0 @@
-int gdbInput;//the fd we write to
-int gdbOutput;//the fd we read from
-int gdbPID;//the pid of gdb, NOT the pid of gdbs debug-e
-
-typedef char *token;//any sequence of digits
-#include "gdbInput.h"
-#include "gdbOutput.h"
